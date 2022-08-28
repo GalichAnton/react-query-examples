7212d8922de0df8af14ee5a37fec63fa14afc127
anton_galich <anton.galich@simbirsoft.com>
2022-08-28 11:15:23 +0300

    setup linters, add styles, traditional fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
 import React from "react";
 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import "./App.css";
+import HomePage from "./pages/Home.page";
+import RqSuperHeroesPage from "./pages/RQSuperHeroes.page";
+import SuperHeroesPage from "./pages/SuperHeroes.page";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/super-heroes">Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <Routes>
+            <Route path="/super-heroes" element={<SuperHeroesPage />} />
+            <Route path="/rq-super-heroes" element={<RqSuperHeroesPage />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/RQSuperHeroes.page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RqSuperHeroesPage = () => {
+  return <div>RQ Super Heroes Page</div>;
+};
+
+export default RqSuperHeroesPage;
